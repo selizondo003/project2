3c394468cd9f8b6f2c7d31c7c5d6ebc8539805a0
sofia <selizondo003@insite.4cd.edu>
2022-04-06 12:35:41 -0700

    beautify & validation

--- j/main.js
+++ j/main.js
@@ -15,65 +15,65 @@ function init() {
   slides.forEach((slide) => {
     slide.classList.add("hide", "abs-pos");
   });
-  
+
   // show the first slide
   slides[0].classList.remove("hide");
 
   //create next and back button controls 
-   next_btn.addEventListener("click", changeSlide);
-   back_btn.addEventListener("click", changeSlide);
+  next_btn.addEventListener("click", changeSlide);
+  back_btn.addEventListener("click", changeSlide);
 
-    //start timer, chages every 4sec
-    myInterval = setInterval(changeSlide, 4000);
-    
-   //create caption
-   caption.innerHTML= frame.firstElementChild.alt;
+  //start timer, chages every 4sec
+  myInterval = setInterval(changeSlide, 4000);
 
-  
+  //create caption
+  caption.innerHTML = frame.firstElementChild.alt;
+
+
 }
 
 
 function changeSlide(e) {
-  
-    //check to see if function was called by button click or setInterval
-    //if the event object passes, that means button was clicked and we will turn off the timer
-    if (e) {
-        e.preventDefault();
-        clearInterval(myInterval);
-    }
-    
-    //shortcut vars
-    const frame = document.querySelector(".frame");
-    const slides = frame.querySelectorAll("img");
-    const caption = document.querySelector(".caption");
-    let showing = document.querySelector(".current");
-    let nextUp = "";
-  
-    //determine what direction slides are going in
-    //if myInterval is called, no event obejct is passed
-    if(!e || e.target.className == "next-btn") {
-        nextUp = showing.nextElementSibling;
-        } else {
-            nextUp = showing.previousElementSibling;
-        }
-    
-    // deactivate current image
-    showing.classList.toggle("hide");
-    showing.classList.toggle("current");
-    
-    //make sure next image is there
-    if (!nextUp) {
-        nextUp = slides[slides.length - 1];
-    }
 
-    if (nextUp.nodeName !== "IMG") {
-      nextUp = slides[0];
-    }
-  
-    // activate next image
-    nextUp.classList.toggle("hide");
-    nextUp.classList.toggle("current");
+  //check to see if function was called by button click or setInterval
+  //if the event object passes, that means button was clicked and we will turn off the timer
+  if (e) {
+    e.preventDefault();
+    clearInterval(myInterval);
+  }
 
-    //change caption text
-    caption.innerHTML = nextUp.alt;
+  //shortcut vars
+  const frame = document.querySelector(".frame");
+  const slides = frame.querySelectorAll("img");
+  const caption = document.querySelector(".caption");
+  let showing = document.querySelector(".current");
+  let nextUp = "";
+
+  //determine what direction slides are going in
+  //if myInterval is called, no event obejct is passed
+  if (!e || e.target.className == "next-btn") {
+    nextUp = showing.nextElementSibling;
+  } else {
+    nextUp = showing.previousElementSibling;
+  }
+
+  // deactivate current image
+  showing.classList.toggle("hide");
+  showing.classList.toggle("current");
+
+  //make sure next image is there
+  if (!nextUp) {
+    nextUp = slides[slides.length - 1];
+  }
+
+  if (nextUp.nodeName !== "IMG") {
+    nextUp = slides[0];
+  }
+
+  // activate next image
+  nextUp.classList.toggle("hide");
+  nextUp.classList.toggle("current");
+
+  //change caption text
+  caption.innerHTML = nextUp.alt;
 }
